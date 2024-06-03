aab8e16e51d1f0af608551b5cb1df86d1d064adb
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2024-06-03 08:14:03 +0500

    Add files via upload

---
+++ Q5/main.js
@@ -0,0 +1,8 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+var famousperson = "Albert Einstein";
+var famousqoute = "A person who never made a mistake never tried anything new.";
+var message = famousperson + ' once said, "A person who never made a mistake never tried anything new."';
+console.log(message);
+// console.log(`${famousperson} once said , ${famousqoute}`);
