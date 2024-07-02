d98d6ace6aae11bbd9800a810911cd1f24305ba2
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2024-07-02 07:37:35 +0500

    Delete Q3 directory

--- Q3/Q3.ts
+++
@@ -1,9 +0,0 @@
-// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-let personName = "Naseer ahmed";
-
-console.log(personName.toLowerCase());
-
-console.log(personName.toUpperCase());
-
-console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()));
