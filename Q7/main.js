83e1f323ea570c9ad58154d9769b908d58563885
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2024-07-02 07:36:37 +0500

    Delete Q7 directory

--- Q7/main.js
+++
@@ -1,9 +0,0 @@
-// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-// Addition :
-console.log("Addition result", 4 + 4);
-// Substraction :
-console.log("Substraction result", 10 - 2);
-// Multiplication :
-console.log("Multiplication result", 4 * 2);
-// Division
-console.log("Division result", 16 / 2);
