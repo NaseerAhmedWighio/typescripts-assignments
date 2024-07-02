dc9d42928afbe54f400bd3f1962512429772e50d
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2024-07-02 07:37:48 +0500

    Delete Q4 directory

--- Q4/Q4.js
+++
@@ -1,5 +0,0 @@
-//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-var author = "Albert Einestin";
-var qoute = "A person who never made a mistake never tried anything new.";
-console.log("".concat(author, " once said ,\"").concat(qoute, "\""));
--- Q4/Q4.ts
+++
@@ -1,8 +0,0 @@
-//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-
-//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-
-let author:string = "Albert Einestin";
-let qoute :string ="A person who never made a mistake never tried anything new.";
-
-console.log(`${author} once said ,"${qoute}"`);
