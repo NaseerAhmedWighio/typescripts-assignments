05716de1b15ca39d92a1c5912af6ae01cb6fd0c6
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2024-07-02 07:36:58 +0500

    Delete Q8 directory

--- Q8/main.js
+++
@@ -1,7 +0,0 @@
-// You should create four lines that look like this:
-// console.log(5 + 3)
-// Your output should simply be four lines with the number 8 appearing once on each line.
-console.log("(4+4) = ", 4 + 4);
-console.log("(10-2) = ", 10 - 2);
-console.log("(4*2) = ", 4 * 2);
-console.log("(16/2) = ", 16 / 2);
