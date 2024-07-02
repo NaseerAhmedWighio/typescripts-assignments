c884f5e8755b938cf19c383ed7da9aa23348dc17
Naseer Ahmed Wighio <159708498+NaseerAhmedWighio@users.noreply.github.com>
2024-07-02 07:35:32 +0500

    Delete Q2 directory

--- Q2/Q2.js
+++
@@ -1,6 +0,0 @@
-// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-var person = "Eric";
-var message = "Would you like to learn some python today?";
-console.log("HI");
-console.log(person);
-console.log(message);
--- Q2/Q2.ts
+++
@@ -1,8 +0,0 @@
-// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-
-
-let person = "Eric"
-let message = "Would you like to learn some python today?"
-console.log("HI");
-console.log(person);
-console.log(message);
